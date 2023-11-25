6dedb11466bc82570eb9325202e53e08feda95b1
flamrdevs <flamrdevs@gmail.com>
2023-11-25 20:19:43 +0700

    chore: initialize app

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import { Hono, HTTPException } from 'hono/mod.ts';
+import { compress, cors } from 'hono/middleware.ts';
+
+const app = new Hono();
+
+app.use('*', cors({ origin: '*' }), compress());
+
+app
+	.get('/', (ctx) => ctx.json({ name: 'e-base-deno' }, 200))
+	.notFound((ctx) => ctx.json({ message: 'Not found' }, 404))
+	.onError((err, ctx) => {
+		if (err instanceof HTTPException) return err.getResponse();
+		let message = 'Internal server error';
+		if (err instanceof Error) message = err.message;
+		return ctx.json({ message }, 500);
+	});
+
+export default app;
