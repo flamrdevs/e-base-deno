5d0becbad40bf1cc36634daaf3d010f95a1bb4ef
flamrdevs <flamrdevs@gmail.com>
2023-12-12 09:52:39 +0700

    chore: env

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import { Hono, HTTPException } from 'hono/mod.ts';
 import { compress, cors } from 'hono/middleware.ts';
 
+import * as v from 'valibot/mod.ts';
+
+const env = v.parse(
+	v.object({
+		APP_NAME: v.string(),
+	}),
+	Deno.env.toObject(),
+);
+
 const app = new Hono();
 
 app.use('*', cors({ origin: '*' }), compress());
 
+app.get('/env', (ctx) => ctx.json(env));
+
 app
-	.get('/', (ctx) => ctx.json({ name: Deno.env.get('APP_NAME') }, 200))
+	.get('/', (ctx) => ctx.json({ name: env.APP_NAME }, 200))
 	.notFound((ctx) => ctx.json({ message: 'Not found' }, 404))
 	.onError((err, ctx) => {
 		if (err instanceof HTTPException) return err.getResponse();
